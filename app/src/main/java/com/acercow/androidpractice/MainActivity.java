60fd98fc93682786dcdabc7936b75d669bf63e72
zhaosen <zhaosen@staff.weibo.com>
2017-09-25 17:14:12 +0800

    immersive mode

--- app/src/main/java/com/acercow/androidpractice/MainActivity.java
+++ app/src/main/java/com/acercow/androidpractice/MainActivity.java
@@ -1,21 +1,95 @@
 package com.acercow.androidpractice;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.acercow.androidpractice.gesture.GestureTestActivity;
 import com.acercow.androidpractice.viewpager.ScreenSlidePageActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private View mDecorView;
+    private EditText et;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        mDecorView = getWindow().getDecorView();
+
+
+        final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("title");
+
+
+        setContentView(R.layout.activity_main);
+        et = (EditText) findViewById(R.id.et_test);
+        findViewById(R.id.et_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard();
+            }
+        });
 //        startActivity(new Intent(this, GestureTestActivity.class));
-        startActivity(new Intent(this, ScreenSlidePageActivity.class));
-        finish();
+//        startActivity(new Intent(this, ScreenSlidePageActivity.class));
+//        finish();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hidestickySystemUI();
+        }
+        Log.d(TAG, "onWindowFocusChanged :: hasFocus :: " + hasFocus);
+    }
+
+    private void hidestickySystemUI() {
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(et, InputMethodManager.SHOW_FORCED);
+        imm.hideSoftInputFromWindow(et.getWindowToken(), 0); //强制隐藏键盘
+    }
+
+    // This snippet hides the system bars.
+    private void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+
+    // This snippet shows the system bars. It does this by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
