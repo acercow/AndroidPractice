14499109f4a0ed7d0525a895acd06661e4b16c2c
zhaosen <zhaosen@staff.weibo.com>
2017-12-07 15:58:23 +0800

    [comments]测试

--- app/src/main/java/com/acercow/androidpractice/MainActivity.java
+++ app/src/main/java/com/acercow/androidpractice/MainActivity.java
@@ -1,20 +1,20 @@
 package com.acercow.androidpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.acercow.androidpractice.lifecycle.LifeCycleActivity;
 import com.acercow.androidpractice.rankview.HourRankingHeaderView;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class MainActivity extends AppCompatActivity {
-
-    int i = 10;
+    private static final String TAG = "[LifeCycle]" + "[Main]";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((HourRankingHeaderView) findViewById(R.id.ranking)).updateContent(" 另撒娇扥拉斯看扥令爱三扥 ");
@@ -25,9 +25,9 @@ public class MainActivity extends AppCompatActivity {
 //                WebView web = new XiaoSenWebView(MainActivity.this);
 //                Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.web);
 //                web.startAnimation(anim);
-//                startActivity(new Intent(MainActivity.this, LifeCycleActivity.class));
-                HourRankingDialog dialog = new HourRankingDialog(MainActivity.this, R.style.CommonBottomDialog);
-                dialog.show();
+                startActivity(new Intent(MainActivity.this, LifeCycleActivity.class));
+//                HourRankingDialog dialog = new HourRankingDialog(MainActivity.this, R.style.CommonBottomDialog);
+//                dialog.show();
 
             }
         });
@@ -67,5 +67,36 @@ public class MainActivity extends AppCompatActivity {
 //                return false;
 //            }
 //        });
+    }
+
+
+    @Override
+    public void onStart() {
+        Log.v(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.v(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.v(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.v(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.v(TAG, "onDestroy");
+        super.onDestroy();
     }
 }
